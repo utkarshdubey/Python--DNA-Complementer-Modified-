e844e2c62f961e9eb0066dd883da9a08dec226d4
Utkarsh Dubey <utkarsh.dubey3jan@gmail.com>
2018-08-16 17:43:09 +0530

    Little work done.

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import re
-
 while True:
     print("\n")
-    inp = list(input("Please enter a DNA Sequence: "))
+    inp = str(input("Please enter a DNA Sequence: "))
+    chars = re.findall(r'[GCATgcat]', inp)
+    empty_list = []
+    for char in chars:
+        if char == "G":
+            empty_list.append("C")
+        if char == "C":
+            empty_list.append("G")
+        if char == "A":
+            empty_list.append("T")
+        if char == "T":
+            empty_list.append("A")
+    print(''.join(empty_list))
